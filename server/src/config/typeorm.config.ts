d79bc0eaa240951c22778f78c9f7da3979c424b6
Ignacio Joaquín Soler <ignaciojsoler@gmail.com>
2024-09-04 03:19:06 -0300

    Feat: Add migration commands in package.json

--- server/src/config/typeorm.config.ts
+++ server/src/config/typeorm.config.ts
@@ -4,5 +4,5 @@ export default new DataSource({
   type: 'sqlite',
   database: 'database/database.sqlite',
   entities: ['src/**/*.entity{.ts,.js}'],
-  migrations: ['src/database/migrations/*{.ts,.js}'],
+  migrations: ['database/migrations/*{.ts,.js}'],
 });
