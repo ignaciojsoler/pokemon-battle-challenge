5e8bb52b66b9c761ca970b65f97cc47d5f7c44ea
Ignacio Joaquín Soler <ignaciojsoler@gmail.com>
2024-09-04 02:45:55 -0300

    Feat: Add pokemon and battle resources

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { BattleModule } from './battle/battle.module';
 
 @Module({
-  imports: [],
+  imports: [PokemonModule, BattleModule],
   controllers: [AppController],
   providers: [AppService],
 })
