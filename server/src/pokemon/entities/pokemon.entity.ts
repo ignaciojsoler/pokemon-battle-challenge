240b9b8161d30a6f835929304afe2b8e89953acb
Ignacio Joaquín Soler <ignaciojsoler@gmail.com>
2024-09-04 03:09:53 -0300

    Feat: Update pokemon entity

--- server/src/pokemon/entities/pokemon.entity.ts
+++ server/src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,28 @@
-export class Pokemon {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('pokemon')
+export class Pokemon {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  attack: number;
+
+  @Column()
+  defense: number;
+
+  @Column()
+  hp: number;
+
+  @Column()
+  speed: number;
+
+  @Column()
+  type: string;
+
+  @Column({ nullable: true })
+  imageUrl?: string;
+}
