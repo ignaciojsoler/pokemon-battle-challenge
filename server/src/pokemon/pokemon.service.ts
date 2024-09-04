98536b7df9dbc87381924d145ca0086f7f5868bd
Ignacio Joaquín Soler <ignaciojsoler@gmail.com>
2024-09-04 03:48:08 -0300

    Feat: Add seed pokemons endpoint and clear dtos

--- server/src/pokemon/pokemon.service.ts
+++ server/src/pokemon/pokemon.service.ts
@@ -1,26 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { CreatePokemonDto } from './dto/create-pokemon.dto';
-import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Pokemon } from './entities/pokemon.entity';
+import { Repository } from 'typeorm';
+import { pokemonList } from 'src/seeds/pokemon.seed';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
-    return 'This action adds a new pokemon';
-  }
+
+  constructor(
+    @InjectRepository(Pokemon) private pokemonRepository: Repository<Pokemon>
+  ) {}
 
   findAll() {
-    return `This action returns all pokemon`;
+    return this.pokemonRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
-  }
-
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} pokemon`;
+  async seedPokemons() {
+    await this.pokemonRepository.clear();
+    return this.pokemonRepository.save(pokemonList);
   }
 }
